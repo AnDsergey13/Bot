1141ccb83500dea6dd3b2c157193a651754233af
AnDsergey13 <serega.andrushko@gmail.com>
2018-11-14 13:22:58 +0300

    Выше 7в7в7 не берёт

--- example/ex6_for.py
+++ example/ex6_for.py
@@ -5,6 +5,7 @@ for j in [1,2,3,4,5,6,7,8,9,10]:
 	#print(j**j, end = "_")
 		j = j**(j**j)
 		#print("{}".format(j))
+
 		my_file = open('10^10^10.txt', 'w')
 		my_file.write(str(j))
 		my_file.close()
