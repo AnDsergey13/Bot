f55e00833383196290e65e759f796924b5685e4d
AnDsergey13 <serega.andrushko@gmail.com>
2018-11-14 05:50:54 +0300

    Эксперименты с числами в степени и запись их в файл

--- example/ex6_for.py
+++ example/ex6_for.py
@@ -1,9 +1,19 @@
-# i = int(input("Введите ваше число: "))
+i = int(input("Введите число от 1 до 10: "))
 
-for j in "Hello world":
-	if j == "D":
-		break
-	print(j*2, end = "")
-print("")
+for j in [1,2,3,4,5,6,7,8,9,10]:
+	if j == i:
+	#print(j**j, end = "_")
+		j = j**(j**j)
+		#print("{}".format(j))
+		my_file = open('10^10^10.txt', 'w')
+		my_file.write(str(j))
+		my_file.close()
+
+'''
 else:
-	print("Сработал break")
+	print("Попробуйте ещё раз")
+
+
+else:
+	print("Вы ввели число{}".format())
+'''
