8dd9b803823e17c2f0684cb34dc1a62c6987e358
Live session user <serega.andrushko@gmail.com>
2018-12-02 08:04:06 +0300

    Оптимизация среза.Удаление библиотеки copy.

--- Bot/method_trades.py
+++ Bot/method_trades.py
@@ -1,7 +1,8 @@
 from binance_api import Binance
+import threading
 import pdb
 import time
-import copy # Нужен ли этот модуль вообще?
+import json
 #pdb.set_trace()
 
 bot = Binance(
@@ -9,57 +10,62 @@ bot = Binance(
     API_SECRET=''
 )
 
-period_record = 1	#Сколько производить запись в минутах
-delay = 20	#задержка между двумя запросми (по умолчанию)(в секундах)
+period_record = 20	#Сколько производить запись в секундах
+delay = 5	#задержка между двумя запросми (по умолчанию)(в секундах)
 
 def record_in_file():	#запись фильтрованных значений
+	#print(range_for_record)
 	for string in range_for_record:
 		test_data.write(str(string))
-		test_data.write("\n")
-		#print(string) 
-
+		test_data.write(",\n")
+		
 def request_time():
 	current_time = bot.time()
 	current_time = int(current_time['serverTime'])
 	return current_time
 
-test_data = open("statistic_data.txt", "w")
-first_request = bot.trades(symbol='BTCUSDT', limit = 500)
-range_for_record = copy.deepcopy(first_request)	#!!!
+def countdown(end_record):		#параллельный поток для показа времени
+	while 1:
+		current_time = request_time()
+		ostatok = (end_record - current_time)/1000
+		if current_time >= end_record:
+			break
+		print("Осталось = {o:0.1f} секунд".format(o=ostatok))
+		time.sleep(0.4)
+
+
+test_data = open("statistic_data.json", "w")
+first_request = bot.trades(symbol='BTCUSDT', limit = 300)
+range_for_record = first_request.copy()
+
 record_in_file()			
 del range_for_record							#!!!
 
-period_in_second = period_record * 60 *	1000 #перевод в милисекунды для binance
-#start_record = int(round(time.time(),0))*1000 
+period_in_second = period_record * 1000 #перевод в милисекунды для binance
 start_record = request_time()
 end_record = start_record + period_in_second
-#print(start_record, " ", end_record)
-#print(end_record - start_record)
 
 test_string = first_request[-1]
+
+t = threading.Thread(target=countdown, args=(end_record,))
+t.start()
+
 
 while 1:
 	current_time = request_time()
 
-	#print("PC     ",int(round(time.time(),0)))
-	#print("end_record 	  = ", end_record)
-	#rint("B_current_time = ", current_time)
-	ostatok = (end_record - current_time)/1000
-	print("Продолжаем, осталось = ", int(ostatok))
-
 	if current_time < end_record:
 		time.sleep(delay)
-		second_request = bot.trades(symbol='BTCUSDT', limit = 500)
+		second_request = bot.trades(symbol='BTCUSDT', limit = 300)
 
-		last_index_sec_req = len(second_request)	# номер последней строки +1
-		#(!)
+		#last_index_sec_req = len(second_request)	# номер последней строки +1
 		num_str_sec_req = 0
 
 		for string_sec_req in second_request:
 			if test_string == string_sec_req:	#нахождение строки (2 list), на которой закончился прошлый запрос (1 list)
 				num_str_sec_req += 1			#выставляем номер строки для начала записи (след. строка после обнаруженной)
 				#itr = iter(num_list)
-				range_for_record = second_request[num_str_sec_req:last_index_sec_req]
+				range_for_record = second_request[num_str_sec_req:]
 				test_string = range_for_record[-1]	#при следующих записях, обновляет проверочную строку во избежания сдвига при записи
 				record_in_file()
 				del range_for_record
@@ -71,6 +77,8 @@ while 1:
 		print("Конец записи")
 		break
 test_data.close()
+
+
 '''
 tt = bot.time()
 print("Binance",tt['serverTime'])	#время биржи отстаёт, в среднем, на 34 секунды
